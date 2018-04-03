2742b023e389697ed82bebba5c5a5b49307f52fe
Vladimir D'Ruzh <dialog.txt@gmail.com>
2018-04-03 22:25:28 +0700

    Done: 12 level

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1110/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task11.task1110;
+
+/* 
+Не забываем инкапсулировать
+1. В классе Cat должно быть поле типа String с именем name.
+2. В классе Cat должно быть поле типа int с именем age.
+3. В классе Cat должно быть поле типа int с именем weight.
+4. В классе Cat должно быть поле типа int с именем speed.
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public Cat(String name, int age, int weight) {
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() { return age; }
+
+        public void setWeight(int weight) {
+
+        }
+
+        public void setSpeed(int speed) {
+
+        }
+    }
+}
